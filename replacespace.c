93d0ed2357a1f42841e25a61027f032171e152b8
ayushrawat2007124-max <ayushrawat2007124@gmail.com>
2025-11-03 10:10:13 +0530

    program

---
+++ replacespace.c
@@ -0,0 +1,26 @@
+/*
+Q88: Replace spaces with hyphens in a string
+Sample Test Cases:
+Input 1:
+hello world
+Output 1:
+hello-world
+*/
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+int main() {
+    char str[100];
+
+    printf("Enter a string:");
+    fgets(str, sizeof(str), stdin);
+
+    for (int i = 0; str[i] != '\0'; i++) {
+        if (isspace(str[i])) {
+            str[i] = '-';
+        }
+    }
+
+    printf("Modified string: %s\n", str);
+    return 0;
+}
